5bd2828e482970daa587c06f2e4e7eed941af24a
Ike Addy <ikaddy05@gmail.com>
2024-02-21 20:58:03 -0500

    Added window resize event listener to scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const WORLD_WIDTH;
+const WORLD_HEIGHT;
+
+const worldElement = document.querySelector('[data-world]');
+
+setPixelToWorldScale()
+window.addEventListener('resize', setPixelToWorldScale);
