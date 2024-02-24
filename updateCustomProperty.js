726f4b97d01aaaf1bf1932b232e817015aaa0312
Ike Addy <ikaddy05@gmail.com>
2024-02-23 22:04:31 -0500

    Added setCustomProperty function

--- updateCustomProperty.js
+++ updateCustomProperty.js
@@ -1,9 +1,11 @@
+// Gets css property and converts to number
 export function getCustomProperty (element, property) {
     return parseFloat(getComputedStyle(element).getPropertyValue(property)) || 0;
 }
 
-export function setCustomProperty () {
-
+// Gives css property a new value
+export function setCustomProperty (element, property, value) {
+    element.style.setProperty(property, value)
 }
 
 export function incrementCustomProperty () {
