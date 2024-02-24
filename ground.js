c366978651e2d8e8d1893b5c3795c50824a7f375
Ike Addy <ikaddy05@gmail.com>
2024-02-23 22:27:10 -0500

    Added infinite ground

--- ground.js
+++ ground.js
@@ -1,10 +1,23 @@
-import { incrementCustomProperty } from "./updateCustomProperty.js";
+import { 
+    getCustomProperty, 
+    incrementCustomProperty, 
+    setCustomProperty 
+} from "./updateCustomProperty.js";
 
 const groundElements = document.querySelectorAll('[data-ground]');
-
 const SPEED = .05;
+
+export function setGround() {
+    setCustomProperty(groundElements[0], '--left', 0);
+    setCustomProperty(groundElements[1], '--left', 300);
+}
+
 export function updateGround(delta) {
     groundElements.forEach(ground => {
         incrementCustomProperty(ground, '--left', delta * SPEED * -1);
+
+        if (getCustomProperty(ground, '--left') <= -300) {
+            incrementCustomProperty(ground, '--left', 600);
+        }
     })
 }
--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-import { updateGround } from './ground.js';
+import { setGround, updateGround } from './ground.js';
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
@@ -7,6 +7,8 @@ const worldElement = document.querySelector('[data-world]');
 
 setPixelToWorldScale()
 window.addEventListener('resize', setPixelToWorldScale);
+
+setGround();
 
 function setPixelToWorldScale() {
     let worldToPixelScale
