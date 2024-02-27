81b14964c461f441b6c5ecb384f7ab3cf355d824
Ike Addy <ikaddy05@gmail.com>
2024-02-26 19:40:22 -0500

    Created a variable for current frame time to make dino legs move

--- dino.js
+++ dino.js
@@ -6,9 +6,12 @@ const FRAME_TIME = 100;
 
 let isJumping;
 let dinoFrame;
+let currentFrameTime;
+
 export function setupDino() {
     isJumping = false;
     dinoFrame = 0;
+    currentFrameTime = 0;
 }
 
 export function updateDino(delta, speedScale) {
