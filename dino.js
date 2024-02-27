2a2a5ca56295d04ab1c9fde29fc3a63ad228e8b0
Ike Addy <ikaddy05@gmail.com>
2024-02-26 19:26:57 -0500

    Added necessary variables to dino.js

---
+++ dino.js
@@ -0,0 +1,13 @@
+const dinoElement = document.querySelector('[data-dino]');
+const JUMP_SPEED = .45;
+const gravity = .011;
+const DINO_FRAME_COUNT = 2;
+const FRAME_TIME = 100;
+
+export function setupDino() {
+
+}
+
+export function updateDino(delta, speedScale) {
+
+}
