dcab2f390ef23aa5808cf5f77289f4266c2a0415
Ike Addy <ikaddy05@gmail.com>
2024-02-27 22:56:54 -0500

    Webdev version

--- ground.js
+++ ground.js
@@ -1,23 +1,23 @@
-import { 
-    getCustomProperty, 
-    incrementCustomProperty, 
-    setCustomProperty 
-} from "./updateCustomProperty.js";
-
-const groundElements = document.querySelectorAll('[data-ground]');
-const SPEED = .05;
-
-export function setupGround() {
-    setCustomProperty(groundElements[0], '--left', 0);
-    setCustomProperty(groundElements[1], '--left', 300);
-}
-
-export function updateGround(delta, speedScale) {
-    groundElements.forEach(ground => {
-        incrementCustomProperty(ground, '--left', delta * speedScale * SPEED * -1);
-
-        if (getCustomProperty(ground, '--left') <= -300) {
-            incrementCustomProperty(ground, '--left', 600);
-        }
+import {
+    getCustomProperty,
+    incrementCustomProperty,
+    setCustomProperty,
+  } from "./updateCustomProperty.js"
+  
+  const SPEED = 0.05
+  const groundElems = document.querySelectorAll("[data-ground]")
+  
+  export function setupGround() {
+    setCustomProperty(groundElems[0], "--left", 0)
+    setCustomProperty(groundElems[1], "--left", 300)
+  }
+  
+  export function updateGround(delta, speedScale) {
+    groundElems.forEach(ground => {
+      incrementCustomProperty(ground, "--left", delta * speedScale * SPEED * -1)
+  
+      if (getCustomProperty(ground, "--left") <= -300) {
+        incrementCustomProperty(ground, "--left", 600)
+      }
     })
-}
+  }
--- updateCustomProperty.js
+++ updateCustomProperty.js
@@ -1,14 +1,11 @@
-// Gets css property and converts to number
-export function getCustomProperty (element, property) {
-    return parseFloat(getComputedStyle(element).getPropertyValue(property)) || 0;
-}
-
-// Gives css property a new value
-export function setCustomProperty (element, property, value) {
-    element.style.setProperty(property, value)
-}
-
-// Getting the current value, adding to it, and setting the value
-export function incrementCustomProperty (element, property, increment) {
-    setCustomProperty(element, property, getCustomProperty(element, property) + increment);
-}
+export function getCustomProperty(elem, prop) {
+    return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0
+  }
+  
+  export function setCustomProperty(elem, prop, value) {
+    elem.style.setProperty(prop, value)
+  }
+  
+  export function incrementCustomProperty(elem, prop, inc) {
+    setCustomProperty(elem, prop, getCustomProperty(elem, prop) + inc)
+  }
